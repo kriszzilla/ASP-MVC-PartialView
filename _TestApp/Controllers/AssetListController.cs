bcb2c1e84563f935ccece89deb38e7cf00c4585a
Kriszzilla <kristian.aggarao@gmail.com>
2017-08-21 22:08:15 +0800

    Source Code
    Source Code Tutorial for ASP.NET Partial View

---
+++ _TestApp/Controllers/AssetListController.cs
@@ -0,0 +1,47 @@
+﻿using _TestApp.Infra;
+using _TestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _TestApp.Controllers
+{
+    public class AssetListController : Controller
+    {
+        DataContext context;
+
+        public AssetListController()
+        {
+            this.context = new DataContext(Connections.connection);
+        }
+
+
+        // GET: AssetList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult ListOfBooks(int varID, string varDescription)
+        {
+            IList<Book> AllBooks = context.GetTable<Book>().ToArray();
+            return PartialView(AllBooks.Where(x => x.BookId.ToString().Contains(varID.ToString()) && x.Description.Contains(varDescription) ));
+        }
+
+        public PartialViewResult ListOfPens(int varID, string varDescription)
+        {
+            IList<Pen> AllPens = context.GetTable<Pen>().ToArray();
+            return PartialView(AllPens.Where(x => x.PenId.ToString().Contains(varID.ToString()) && x.Description.Contains(varDescription)));
+        }
+
+        public PartialViewResult ListOfToys(int varID, string varDescription)
+        {
+            IList<Toy> AllToys = context.GetTable<Toy>().ToArray();
+            return PartialView(AllToys.Where(x => x.ToyId.ToString().Contains(varID.ToString()) && x.Description.Contains(varDescription)));
+        }
+
+    }
+}
---
+++ _TestApp/Infra/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _TestApp.Infra
+{
+    public class DBConnection
+    {
+    }
+
+    public class Connections
+    {
+        public static string connection = @"Data Source=DESKTOP-4C0C5AN\SQLSERVER;Initial Catalog=TestAssetDB;Integrated Security=True;User ID=testlog;Password=testlog";
+    }
+}
